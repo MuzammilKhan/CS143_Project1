c7b0eff6bae6c3620d16c23f5f1ac05af8d12f36
Muzammil <muzammil.a.khan@live.com>
2016-10-16 00:52:27 -0700

    bugfix: changed tab to spaces in create.sql

--- Project1B/create.sql
+++ Project1B/create.sql
@@ -1,68 +1,66 @@
---- Note: Need to add comments describing constraints
---- Add Check statements
 CREATE TABLE Movie (
-	id INT NOT NULL,
-	title VARCHAR(100) NOT NULL,
-	year INT NOT NULL,
-	rating VARCHAR(10),
-	company VARCHAR(50),
-	PRIMARY KEY(id)
+       id INT NOT NULL,
+       title VARCHAR(100) NOT NULL,
+       year INT,
+       rating VARCHAR(10),
+       company VARCHAR(50),
+       PRIMARY KEY(id)
 ) ENGINE=INNODB;
 
 CREATE TABLE Actor (
-	id INT NOT NULL,
-	last VARCHAR(20), 
-	first VARCHAR(20), 
-	sex VARCHAR(6),
-	dob DATE NOT NULL, 
-	dod DATE,
-	PRIMARY KEY(id)
+       id INT NOT NULL,
+       last VARCHAR(20), 
+       first VARCHAR(20), 
+       sex VARCHAR(6),
+       dob DATE NOT NULL, 
+       dod DATE,
+       PRIMARY KEY(id)
 ) ENGINE=INNODB;
 
 CREATE TABLE Director (
-	id INT NOT NULL,
-	last VARCHAR(20),
-	first VARCHAR(20),
-	sex VARCHAR(6),
-	dob DATE NOT NULL,
-	dod DATE,
-	PRIMARY KEY(id)
+       id INT NOT NULL,
+       last VARCHAR(20),
+       first VARCHAR(20),
+       sex VARCHAR(6),
+       dob DATE NOT NULL,
+       dod DATE,
+       PRIMARY KEY(id)
 ) ENGINE=INNODB;
 
 CREATE TABLE MovieGenre (
-	mid INT NOT NULL,
-	genre VARCHAR(20),
-	FOREIGN KEY (mid) REFERENCES Movie(id)
+       mid INT NOT NULL,
+       genre VARCHAR(20),
+       FOREIGN KEY (mid) REFERENCES Movie(id)
 ) ENGINE=INNODB;
 
 CREATE TABLE MovieDirector(
-	mid INT NOT NULL,
-	did INT,
-	FOREIGN KEY (mid) REFERENCES Movie(id),
-	FOREIGN KEY (did) REFERENCES Director(id)
+       mid INT NOT NULL,
+       did INT,
+       FOREIGN KEY (mid) REFERENCES Movie(id),
+       FOREIGN KEY (did) REFERENCES Director(id)
 ) ENGINE=INNODB;
 
 CREATE TABLE MovieActor(
-	mid INT NOT NULL,
-	aid INT,
-	role VARCHAR(50),
-	FOREIGN KEY (mid) REFERENCES Movie(id),
-	FOREIGN KEY (aid) REFERENCES Actor(id)
+       mid INT NOT NULL,
+       aid INT,
+       role VARCHAR(50),
+       FOREIGN KEY (mid) REFERENCES Movie(id),
+       FOREIGN KEY (aid) REFERENCES Actor(id)
 ) ENGINE=INNODB;
 
 CREATE TABLE Review(
-	name VARCHAR(20),
-	time TIMESTAMP,
-	mid INT NOT NULL,
-	rating INT,
-	comment VARCHAR(500),
-	FOREIGN KEY (mid) REFERENCES Movie(id)
+       name VARCHAR(20),
+       time TIMESTAMP,
+       mid INT NOT NULL,
+       rating INT,
+       comment VARCHAR(500),
+       FOREIGN KEY (mid) REFERENCES Movie(id)
 ) ENGINE=INNODB;
 
-MaxPersonID(
+CREATE TABLE MaxPersonID(
 	id INT NOT NULL
 ) ENGINE=INNODB;
 
-MaxMovieID(
+CREATE TABLE MaxMovieID(
 	id INT NOT NULL
 ) ENGINE=INNODB;

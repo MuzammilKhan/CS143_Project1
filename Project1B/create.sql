813ae5664e342761edcb139bf023264e6549e567
saadnsyed <saadnsyed@gmail.com>
2016-10-18 02:12:08 -0700

    Added checks and comments to create.sql

--- Project1B/create.sql
+++ Project1B/create.sql
@@ -4,7 +4,10 @@ CREATE TABLE Movie (
        year INT,
        rating VARCHAR(10),
        company VARCHAR(50),
-       PRIMARY KEY(id)
+       # A movie can be uniquely identified by its ID
+       PRIMARY KEY(id),
+       # Ratings must conform to the standard MPAA values
+       CHECK (rating IS NULL OR rating='G' OR rating='PG' OR rating='PG-13' OR rating='R' OR rating='NC-17')
 ) ENGINE=INNODB;
 
 CREATE TABLE Actor (
@@ -14,7 +17,10 @@ CREATE TABLE Actor (
        sex VARCHAR(6),
        dob DATE NOT NULL, 
        dod DATE,
-       PRIMARY KEY(id)
+       # An actor can be uniquely identified by his/her ID
+       PRIMARY KEY(id),
+       # The DOB must be before/same as the DOD
+       CHECK (dod IS NULL OR dod >= dob)
 ) ENGINE=INNODB;
 
 CREATE TABLE Director (
@@ -24,19 +30,25 @@ CREATE TABLE Director (
        sex VARCHAR(6),
        dob DATE NOT NULL,
        dod DATE,
-       PRIMARY KEY(id)
+       # A director can be uniquely identified by his/her ID
+       PRIMARY KEY(id),
+       # The DOB must be before/same as the DOD
+       CHECK (dod IS NULL OR dod >= dob)
 ) ENGINE=INNODB;
 
 CREATE TABLE MovieGenre (
        mid INT NOT NULL,
        genre VARCHAR(20),
+       # The movie ID here must exist in the Movie table
        FOREIGN KEY (mid) REFERENCES Movie(id)
 ) ENGINE=INNODB;
 
 CREATE TABLE MovieDirector(
        mid INT NOT NULL,
        did INT,
+       # The movie ID here must exist in the Movie table
        FOREIGN KEY (mid) REFERENCES Movie(id),
+       # The director ID here must exist in the Director table
        FOREIGN KEY (did) REFERENCES Director(id)
 ) ENGINE=INNODB;
 
@@ -44,7 +56,9 @@ CREATE TABLE MovieActor(
        mid INT NOT NULL,
        aid INT,
        role VARCHAR(50),
+       # The movie ID here must exist in the Movie table
        FOREIGN KEY (mid) REFERENCES Movie(id),
+       # The actor ID here must exist in the Actor table
        FOREIGN KEY (aid) REFERENCES Actor(id)
 ) ENGINE=INNODB;
 
@@ -54,7 +68,10 @@ CREATE TABLE Review(
        mid INT NOT NULL,
        rating INT,
        comment VARCHAR(500),
-       FOREIGN KEY (mid) REFERENCES Movie(id)
+       # The movie ID here must exist in the Movie table
+       FOREIGN KEY (mid) REFERENCES Movie(id),
+       # The rating must be a value between 0 and 5 inclusive
+       CHECK (rating IS NULL OR rating >= 0 AND rating <= 5)
 ) ENGINE=INNODB;
 
 CREATE TABLE MaxPersonID(

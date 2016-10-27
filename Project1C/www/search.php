512a40ad9733224e3dd32955d5e1dd0ef508e32e
Muzammil <muzammil.a.khan@live.com>
2016-10-26 23:00:07 -0700

    Added basic sidebar to search.php

---
+++ Project1C/www/search.php
@@ -0,0 +1,96 @@
+<!-- Used w3 schools example heavily to create sidebar: http://www.w3schools.com/howto/howto_js_sidenav.asp -->
+<!DOCTYPE html>
+<html>
+<style>
+body {
+    font-family: "Lato", sans-serif;
+    transition: background-color .5s;
+}
+
+.sidenav {
+    height: 100%;
+    width: 0;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #111;
+    overflow-x: hidden;
+    transition: 0.5s;
+    padding-top: 60px;
+}
+
+.sidenav h1{
+    color: #f1f1f1;
+    font-size: 28px;
+    text-indent: 30px;
+}
+
+.sidenav a {
+    padding: 8px 8px 8px 32px;
+    text-decoration: none;
+    font-size: 25px;
+    color: #818181;
+    display: block;
+    transition: 0.3s
+}
+
+.sidenav a:hover, .offcanvas a:focus{
+    color: #f1f1f1;
+}
+
+.sidenav .closebtn {
+    position: absolute;
+    top: 0;
+    right: 25px;
+    font-size: 36px;
+    margin-left: 50px;
+}
+
+#main {
+    transition: margin-left .5s;
+    padding: 16px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+}
+</style>
+<body>
+
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#"><h3>Search</h3></a>
+  <h1>Add:</h1>
+  <a href="#">Actor</a>
+  <a href="#">Movie</a>
+  <a href="#">Comments</a>
+  <a href="#">Actor to movie</a>
+  <a href="#">Director to movie</a>
+</div>
+
+<div id="main">
+    <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Menu</span>
+  <h2>Movie Database</h2>
+  <p>Select whether you are searching for actors/actresses or a movie and type in the name in the search bar.</p>
+
+</div>
+
+<script>
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
+</script>
+     
+</body>
+</html>
+

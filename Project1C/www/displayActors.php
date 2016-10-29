09df3b99d234ac02b5c9f7694153656abf53790a
saadnsyed <saadnsyed@gmail.com>
2016-10-29 03:09:23 -0700

    added files that handle showing search results - still geting http 500 error for diplay

--- Project1C/www/displayActors.php
+++ Project1C/www/displayActors.php
@@ -31,7 +31,7 @@
             <div class="inner">
               <h3 class="masthead-brand">CS 143: Movie Database</h3>
               <nav class="nav nav-masthead">
-                <a class="nav-link active" href="#">Search</a>
+                <a class="nav-link" href="#">Search</a>
                 <a class="nav-link" href="#">Add Movie</a>
                 <a class="nav-link" href="#">Add Actor</a>
                 <a class="nav-link" href="#">Add Comments</a>
@@ -41,49 +41,65 @@
             </div>
           </div>
 
-          <!-- <div class="inner cover">
-            <h1 class="cover-heading">Search!</h1>
-             <p class="lead">Type what you are looking for in the search box below. Then select whether you are searching for actors or movies</p> 
-             <p class="lead"> 
-               Makes a big button: <a href="#" class="btn btn-lg btn-secondary">Learn more</a>  
-             </p> 
-          </div> -->
-
-          <script type="text/javascript">
-            function onSubmitForm()
-            {
-              if(document.myform.operation[0].checked == true)
-              {
-                document.myform.action ="displayActors.php";
-              }
-              else
-              if(document.myform.operation[1].checked == true)
-              {
-                document.myform.action ="displayMovies.php";
-              }
-              return true;
-            }
-          </script>
-
-          <form name="myform" onsubmit="return onSubmitForm();" method="GET">
-            <div class="input-group-lg">
-              <input type="radio" name="operation" value="actor" checked>Actor
-              <input type="radio" name="operation" value="movie">Movie
-              <input name="search" type="text" class="form-control" placeholder="Enter here" >
-              <span class="input-group-btn"><button type="submit" class="btn btn-info" type="button">Search</button></span>
-            </div>
-          </form>
-          <div class="mastfoot">
-            <div class="inner">
-              <p>Made by Muzammil Khan and Saad Syed </a>.</p>
-            </div>
+          <div class="inner cover">
+             <h3 class="cover-heading">Actor Search Results</h3> 
+            <!-- <p class="lead">Type what you are looking for in the search box below. Then select whether you are searching for actors or movies</p> -->
+             <!-- <p class="lead"> -->
+              <!-- Makes a big button: <a href="#" class="btn btn-lg btn-secondary">Learn more</a>  -->
+            <!-- </p> -->
           </div>
 
         </div>
+         
+        </div>
+
 
       </div>
 
     </div>
+
+<?php
+  $search = $_GET["search"];
+  $operation = $_GET["operation"];
+
+  if (!empty(($search))) {
+    $servername = "localhost";
+    $username = "cs143";
+    $password = "";
+    //$dbname = "CS143"; //USE THIS ONE WHEN SUBMITTING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    $dbname = "TEST";
+
+    // Create connection
+    $db = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($db->connect_error) {
+        die("Connection failed: " . $db->connect_error);
+    }
+
+    $tokens = preg_split('/\s+/', $search);
+
+    $sql_query = "SELECT first, last, dob FROM Actor WHERE (first LIKE '%$tokens[0]%' OR last LIKE '%$tokens[0]%')";
+    unset($tokens[0]);
+
+    foreach ($tokens as $token) {
+      $sql_query .= " AND (first LIKE '%$token%' OR last LIKE '%$token%')";
+    }
+
+    $sanitized_query = $db->real_escape_string($sql_query);
+    $query_results = $db->query($sanitized_query);
+
+    while ($row = $query_results->fetch_array(MYSQLI_ASSOC)) {
+      $first = $row["first"];
+      $last = $row["last"];
+      $dob = $row["dob"];
+      echo "<a href=#>".$first." ".$last." (".$dob.")</a><br>";
+    }
+
+    $query_results->free();
+    $db->close();
+  }
+
+?>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
